b7802ff49df7445c70975772bf36d924884f6e94
farzad <farzad.ghlm8@gmail.com>
2023-07-15 23:16:04 -0600

    first commit

---
+++ semester.py
@@ -0,0 +1,26 @@
+class CourseSorter:
+    def __init__(self):
+        self.suggested_lessons = {
+            1: [("mabani", 3), ("wrk mabani", 1), ("PUB_English", 3), ("math1", 4), ("physic1", 3), ("Pub_pesian", 3), ("wrkshp pub", 1)],
+            2: [("java", 3), ("wrk java", 1), ("ghosste", 3), ("math2", 3), ("phycis2", 3), ("expert_eng", 2), ("tafsir_quran", 2), ("tafsirnahj", 2)],
+            3: [("data_struc", 3), ("logic_circle", 3), ("defransel", 3), ("physic_az", 1), ("eslamic1", 2), ("elec_circle", 3), ("wrk_lgicircle", 1), ("wrk_elecircle", 1)],
+            4: [("statistic", 3), ("1-data base", 3), ("artitect_cumputer", 3), ("liner_jebra", 3), ("eslamic2", 2), ("lang_machine", 3), ("az_artitect_cumputer", 1)],
+            5: [("tarbiat_badani", 1), ("culture", 2), ("research", 2), ("operation_sys", 3), ("netwrk_cumptr", 3), ("az_ntwri", 1), ("az_os", 1)],
+            6: [("software1", 3), ("internship", 1), ("ayin_zendeghi", 2), ("falsafe", 2), ("akhlagh", 2), ("compiler", 3), ("programing_lang", 3)],
+            7: [("varzesh1", 1), ("revelthn_eslamic", 2), ("emam", 2), ("last_prject", 3), ("software2", 3), ("softwaretest", 3), ("ux", 3)],
+            8: [("recovery", 3), ("signal", 3), ("accembly", 3), ("optional", 3), ("danesh", 2), ("az_accembly", 1)],
+            9: [],
+            10: [],
+        }
+
+        self.passed_courses = []
+
+    
+
+            
+            
+        
+
+
+    
+        
